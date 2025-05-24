83179b429311bbc1a8f878d7cdf42f0ffcdbea9a
akihito7 <akihitopro7@gmail.com>
2025-05-24 18:00:07 -0300

    feat : added unit tests userRepositoryInMemory

---
+++ src/modules/users/infrastructure/database/in-memory/__tests__/unit/user.repository-in-memory.unit.spec.ts
@@ -0,0 +1,37 @@
+import { UserEntity } from "src/modules/users/domain/entities/user.entity";
+import { UserRepositoryInMemory } from "../../user.repository-in-memory";
+import { UserPropsFactory } from "src/tests/factories/user-props.factory";
+
+describe('UserRepositoryInMemory unit tests', () => {
+  let SUT: UserRepositoryInMemory;
+  let entity: UserEntity;
+
+  beforeAll(() => {
+    SUT = new UserRepositoryInMemory();
+  });
+
+  beforeEach(() => {
+    entity = new UserEntity(UserPropsFactory.create());
+    SUT['items'] = [entity];
+  });
+
+  it('should return the correct user when email exists', () => {
+    const user = SUT.findByEmail(entity.email);
+    expect(user).toBeDefined();
+    expect(user!.toJson()).toStrictEqual(entity.toJson());
+  });
+
+  it('should throw an error when trying to find a user by a non-existent email', () => {
+    expect(() => SUT.findByEmail('emailFake')).toThrow(new Error(`User with this email emailFake not found`));
+  });
+
+  it('should return true when checking if an existing email exists', () => {
+    const emailExists = SUT.emailExists(entity.email);
+    expect(emailExists).toBeTruthy();
+  });
+
+  it('should return false when checking if a non-existent email exists', () => {
+    const emailExists = SUT.emailExists('emailFake');
+    expect(emailExists).toBeFalsy();
+  });
+});
