6007895dbb0078b88e58e85aedb78a5285e2917b
akihito7 <akihitopro7@gmail.com>
2025-05-28 12:05:54 -0300

    refactor : adjusted list like to includes author name too

--- src/modules/likes/application/use-cases/list-like.use-case.ts
+++ src/modules/likes/application/use-cases/list-like.use-case.ts
@@ -1,6 +1,8 @@
 import { BaseUseCaseInterface } from "src/shared/application/use-cases/base-use-case";
 import { LikeRepositoryInterface } from "../../domain/repositories/like.repository.interface";
 import { LikeEntity } from "../../domain/entities/like.entity";
+import { UserRepositoryInterface } from "src/modules/users/domain/repositories/user.repository.interface";
+import { UserEntity } from "src/modules/users/domain/entities/user.entity";
 
 export namespace ListLike {
   export interface Input {
@@ -15,22 +17,43 @@ export namespace ListLike {
     id: string;
     postId: string;
     authorId: string;
+    authorName: string
     createdAt: Date;
   }
 
   export class UseCase implements BaseUseCaseInterface<Input, Output> {
-    constructor(private readonly likeRepository: LikeRepositoryInterface) { }
+
+    constructor(
+      private readonly likeRepository: LikeRepositoryInterface,
+      private readonly userRepository: UserRepositoryInterface
+    ) { }
+
     async execute(input: Input): Promise<Output> {
 
       const { postId } = input;
 
       const likesEntity = await this.likeRepository.findManyByPost(postId);
 
-      return { data: this.toOutput(likesEntity) }
+      return { data: await this.toOutput(likesEntity) }
     }
 
-    private toOutput(likesEntity: LikeEntity[]): LikeEntityOutput[] {
-      return likesEntity.map(likeEntity => likeEntity.toJson())
+    private async toOutput(likesEntity: LikeEntity[]): Promise<LikeEntityOutput[]> {
+      const uniqueAuthorIds = [...new Set(likesEntity.map(like => like.authorId))];
+
+      const users = await Promise.all(
+        uniqueAuthorIds.map(id => this.userRepository.findById(id))
+      );
+
+      const userMap = new Map<string, UserEntity>();
+      users.forEach(user => {
+        if (user) userMap.set(user.id, user);
+      });
+
+      return likesEntity.map(like => ({
+        ...like.toJson(),
+        authorName: userMap.get(like.authorId)?.name || 'Unknown',
+      }));
     }
+
   }
 }
