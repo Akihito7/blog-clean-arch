0cb245cf9eb6141ed3b52c168ab29a573b5adcb4
akihito7 <akihitopro7@gmail.com>
2025-05-30 12:37:49 -0300

    refactor : like entity added method static to validate props

--- src/modules/likes/domain/entities/like.entity.ts
+++ src/modules/likes/domain/entities/like.entity.ts
@@ -1,4 +1,5 @@
 import { BaseEntity } from "src/shared/domain/entities/base.entity";
+import { ConflictError } from "src/shared/domain/errors/conflict.error";
 import { ForbiddenError } from "src/shared/domain/errors/forbidden.error";
 
 interface LikeEntityProps {
@@ -9,7 +10,7 @@ interface LikeEntityProps {
 
 export class LikeEntity extends BaseEntity {
   constructor(protected _props: LikeEntityProps, id?: string) {
-    LikeEntity.verifyIfHasAuthor(_props.authorId);
+    LikeEntity.validate(_props)
     super(_props, id)
     this._props.createdAt = _props.createdAt ?? new Date();
   }
@@ -26,9 +27,13 @@ export class LikeEntity extends BaseEntity {
     return this._props.createdAt!;
   }
 
-  static verifyIfHasAuthor(authorId: string) {
+  static validate({ postId, authorId }: LikeEntityProps) {
     if (!authorId) {
       throw new ForbiddenError('Does not can give like without an account.')
+    }
+
+    if (!postId) {
+      throw new ConflictError('PostId is not provided.')
     }
   }
 
