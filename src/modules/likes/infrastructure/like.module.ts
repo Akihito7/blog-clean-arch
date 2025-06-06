5bcaab2c70a701a6642ef378261fe194f2c6c1df
akihito7 <akihitopro7@gmail.com>
2025-06-06 12:02:28 -0300

    feat : added others use case like module

--- src/modules/likes/infrastructure/like.module.ts
+++ src/modules/likes/infrastructure/like.module.ts
@@ -5,6 +5,10 @@ import { AddLike } from "../application/use-cases/add-like.use-case";
 import { UserRepositoryInterface } from "src/modules/users/domain/repositories/user.repository.interface";
 import { PostRepositoryInterface } from "src/modules/posts/domain/repositories/post.repository.interface";
 import { LikeRepositoryInterface } from "../domain/repositories/like.repository.interface";
+import { CheckLike } from "../application/use-cases/check-like.use-case";
+import { ListLike } from "../application/use-cases/list-like.use-case";
+import { UserRepositoryInMemory } from "src/modules/users/infrastructure/database/in-memory/user.repository-in-memory";
+import { RemoveLike } from "../application/use-cases/remove-like.use-case";
 
 @Module({
   imports: [RepositoriesInMemoryModule],
@@ -16,7 +20,29 @@ import { LikeRepositoryInterface } from "../domain/repositories/like.repository.
         return new AddLike.UseCase(likeRepository, postRepository)
       },
       inject: ['LikeRepository', 'PostRepository']
-    }
+    },
+    {
+      provide: CheckLike.UseCase,
+      useFactory: (likeRepository: LikeRepositoryInterface) => {
+        return new CheckLike.UseCase(likeRepository)
+      },
+      inject: ['LikeRepository', 'PostRepository']
+    },
+    {
+      provide: ListLike.UseCase,
+      useFactory: (likeRepository: LikeRepositoryInterface, userRepository: UserRepositoryInterface) => {
+        return new ListLike.UseCase(likeRepository, userRepository)
+      },
+      inject: ['LikeRepository', 'UserRepository']
+    },
+    {
+      provide: RemoveLike.UseCase,
+      useFactory: (likeRepository: LikeRepositoryInterface, userRepository: UserRepositoryInterface) => {
+        return new RemoveLike.UseCase(likeRepository)
+      },
+      inject: ['LikeRepository']
+    },
+
   ]
 
 })
