397e1fe08bc0d5b42d0813cbb1657b48fd23c5bf
akihito7 <akihitopro7@gmail.com>
2025-06-08 23:10:11 -0300

    feat : added tip entity

---
+++ src/modules/tips/domain/entities/tip.entity.ts
@@ -0,0 +1,48 @@
+import { BaseEntity } from "src/shared/domain/entities/base.entity";
+
+interface TipEntityInterface {
+  title: string;
+  content: string;
+  link?: string;
+  used?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class TipEntity extends BaseEntity {
+  constructor(protected _props: TipEntityInterface, id?: string) {
+    super(_props, id);
+    this._props.used = _props.used ?? false;
+    this._props.createdAt = _props.createdAt ?? new Date();
+    this._props.updatedAt = _props.updatedAt ?? new Date();
+  }
+
+  get title() {
+    return this._props.title;
+  }
+
+  get content() {
+    return this._props.content;
+  }
+
+  get link() {
+    return this._props.link;
+  }
+
+  get used() {
+    return this._props.used;
+  }
+
+  get createdAt() {
+    return this._props.createdAt;
+  }
+
+  get updatedAt() {
+    return this._props.updatedAt;
+  }
+
+  markAsUsed() {
+    this._props.used = true;
+    this._props.updatedAt = new Date();
+  }
+}
