743140fb9af644460e8fd5231709b1ed125fef25
akihito7 <akihitopro7@gmail.com>
2025-06-10 16:17:50 -0300

    feat : added following repository in memory

--- src/modules/follow/domain/repositories/follow.repository.interface.ts
+++ src/modules/follow/domain/repositories/follow.repository.interface.ts
@@ -2,6 +2,8 @@ import { BaseRepository } from "src/shared/domain/repositories/base.repository";
 import { FollowEntity } from "../entities/follow.entity";
 
 export interface FollowRepositoryInteface extends BaseRepository<FollowEntity> {
-  follow: (followEntity: FollowEntity) => Promise<void>
-  unFollow: (requesteredId: string, followed: string) => Promise<void>
+  follow(followEntity: FollowEntity): Promise<void>
+  unFollow(requesteredId: string, followed: string): Promise<void>
+  findFollowers(userId: string): Promise<FollowEntity[] | undefined>
+  findFollowings(userId: string): Promise<FollowEntity[] | undefined>
 }
---
+++ src/modules/follow/infrastructure/database/in-memory/follow.repository-in-memory.ts
@@ -0,0 +1,38 @@
+import { FollowEntity } from "src/modules/follow/domain/entities/follow.entity";
+import { FollowRepositoryInteface } from "src/modules/follow/domain/repositories/follow.repository.interface";
+import { BaseRepositoryInMemory } from "src/shared/domain/repositories/base.repository-in-memory";
+
+export class FollowRepositoryInMemory
+  extends BaseRepositoryInMemory<FollowEntity>
+  implements FollowRepositoryInteface {
+
+  async unFollow(requesterId: string, followedId: string): Promise<void> {
+    const followEntityIndex = this.items.findIndex(
+      followEntity =>
+        followEntity.followerId.toLowerCase() === requesterId.toLowerCase() &&
+        followEntity.followingId.toLowerCase() === followedId.toLowerCase()
+    );
+
+    if (followEntityIndex !== -1) {
+      this.items.splice(followEntityIndex, 1);
+    }
+  }
+
+  async follow(followEntity: FollowEntity) {
+    this.items.push(followEntity);
+  }
+
+  async findFollowers(userId: string): Promise<FollowEntity[]> {
+    return this.items.filter(
+      followEntity => followEntity.followingId.toLowerCase() === userId.toLowerCase()
+    );
+  }
+
+  async findFollowings(userId: string): Promise<FollowEntity[]> {
+    return this.items.filter(
+      followEntity => followEntity.followerId.toLowerCase() === userId.toLowerCase()
+    );
+  }
+
+
+}
