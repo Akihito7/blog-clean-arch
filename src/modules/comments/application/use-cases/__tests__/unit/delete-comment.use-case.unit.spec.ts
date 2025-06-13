4149a2f7e2ddb1c927c896e78bf8e1663b11056b
akihito7 <akihitopro7@gmail.com>
2025-06-13 12:59:13 -0300

    feat : added delete comment use case unit test

--- src/modules/comments/application/use-cases/__tests__/unit/delete-comment.use-case.unit.spec.ts
+++ src/modules/comments/application/use-cases/__tests__/unit/delete-comment.use-case.unit.spec.ts
@@ -1,3 +1,45 @@
+import { CommentRepositoryInMemory } from "src/modules/comments/infrastructure/database/in-memory/repositories/comment.repository-in-memory"
+import { DeleteComment } from "../../delete-comment.use-case";
+import { CommentEntity } from "src/modules/comments/domain/entities/comment.entity";
+
 describe('deleteCommentUseCase unit test', () => {
+  let commentRepository: CommentRepositoryInMemory;
+  let SUT: DeleteComment.UseCase
+
+  beforeEach(() => {
+    commentRepository = new CommentRepositoryInMemory();
+    SUT = new DeleteComment.UseCase(commentRepository);
+  });
+
+  it('should delete comment with successfully', async () => {
+
+    const commentEntity = new CommentEntity({
+      authorId: 'fakeAuthorId',
+      content: 'testing delete comment',
+      postId: 'fakePostID',
+    });
+
+    commentRepository['items'] = [commentEntity];
+
+    await expect(SUT.execute({ commentId: commentEntity.id, requesteredId: 'fakeAuthorId' })).resolves.toBeUndefined();
+  });
+
+  it('should throw error when comment is not found', async () => {
+    await expect(() => SUT.execute({ commentId: 'fakeId', requesteredId: 'janeDoe' })).rejects.toThrow('Comment not found.')
+  })
+
+
+  it('should throw error when comment is not found', async () => {
+    const commentEntity = new CommentEntity({
+      authorId: 'fakeAuthorId',
+      content: 'testing delete comment',
+      postId: 'fakePostID',
+    });
+
+    commentRepository['items'] = [commentEntity];
+
+    await expect(() => SUT.execute({ commentId: commentEntity.id, requesteredId: 'otherId' }))
+      .rejects.toThrow('User dont have permission.')
+  })
 
 })
